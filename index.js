9afff5f7e181bb5cf315622cd979062b5bf76e2a
Tahsin Nurun <nuruntahsin@gmail.com>
2021-04-08 21:37:37 +0600

    005

--- index.js
+++ index.js
@@ -15,6 +15,10 @@ app.use(express.urlencoded({ extended: false }));
 
 const port = 5000;
 
+app.get('/', (req,res) => {
+    res.send('hello from db it is working')
+})
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mbvce.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
